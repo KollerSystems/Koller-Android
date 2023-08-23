4d029acd901ab51217cc5d6cbffad66bc9d1bdce
Marci599 <katona.m.b@gmail.com>
2023-08-23 22:45:14 +0200

    naptár variálás

---
+++ application/shared/src/main/java/com/example/shared/EventPoint.kt
@@ -0,0 +1,49 @@
+package com.example.shared
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
+
+class EventPoint(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
+
+    private var mLabel: String = ""
+    private var mTime: String = ""
+
+    private val textLabel: TextView
+    private val textTime: TextView
+
+    init {
+
+        val typedArray = context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.eventPoint,
+            0, 0
+        )
+
+        try {
+            mLabel = typedArray.getString(R.styleable.eventPoint_text) ?: ""
+            mTime = typedArray.getString(R.styleable.eventPoint_time) ?: ""
+        } finally {
+            typedArray.recycle()
+        }
+
+        View.inflate(context, R.layout.event_point, this)
+
+
+        textLabel = findViewById(R.id.text_label)
+
+        textLabel.text = mLabel
+
+        textTime = findViewById(R.id.text_time)
+
+        textTime.text = mTime
+
+    }
+
+}
