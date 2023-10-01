5780928154f8fc0987f7bc95abd1eecaf65ed0c7
Marci599 <katona.m.b@gmail.com>
2023-10-01 17:15:24 +0200

    chip és dátum intervallum nézet kapcsolatának implementálása

--- application/student/src/main/java/com/norbert/koller/student/fragments/CalendarBaseProgramsFragment.kt
+++ application/student/src/main/java/com/norbert/koller/student/fragments/CalendarBaseProgramsFragment.kt
@@ -1,29 +1,32 @@
 package com.norbert.koller.student.fragments
 
+import android.content.DialogInterface
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.util.Pair
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.chip.Chip
 import com.google.android.material.chip.ChipGroup
+import com.google.android.material.datepicker.MaterialDatePicker
 import com.norbert.koller.shared.BaseViewModel
 import com.norbert.koller.shared.MyApplication
 import com.norbert.koller.shared.SuperCoolRecyclerView
 import com.norbert.koller.shared.api.BaseProgramPagingSource
-import com.norbert.koller.shared.api.UserPagingSource
 import com.norbert.koller.shared.data.FiltersData
 import com.norbert.koller.shared.fragments.bottomsheet.ItemListDialogFragment
 import com.norbert.koller.shared.recycleradapter.BaseProgramRecyclerAdapter
 import com.norbert.koller.shared.recycleradapter.ListItem
-import com.norbert.koller.shared.recycleradapter.UserRecyclerAdapter
 import com.norbert.koller.student.R
-import com.norbert.koller.shared.R as Rs
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import com.norbert.koller.shared.R as Rs
+
 
 class CalendarBaseProgramsFragment : Fragment() {
 
@@ -32,11 +35,51 @@ class CalendarBaseProgramsFragment : Fragment() {
 
     var lengthOption : ArrayList<Boolean> = arrayListOf(false, false)
 
+    private fun setupDrpd(chip : Chip) : MaterialDatePicker<Pair<Long, Long>> {
+
+        val dprdb = MaterialDatePicker.Builder.dateRangePicker()
+
+        if(chip.tag != null){
+            dprdb.setSelection(chip.tag as Pair<Long, Long>)
+        }
+
+        val drpd = dprdb.build()
+
+        drpd.addOnPositiveButtonClickListener {selection ->
+
+            var stringForChip = SimpleDateFormat(MyApplication.shortMonthDayFormat).format(selection.first)
+            if(selection.first != selection.second){
+                stringForChip += " - ${SimpleDateFormat(MyApplication.shortMonthDayFormat).format(selection.second)}"
+            }
+            chip.text = stringForChip
+            chip.tag = selection
+            chip.isCheckable = true
+            chip.isChecked = true
+            chip.isCheckable = false
+            chip.closeIcon = AppCompatResources.getDrawable(requireContext(), com.norbert.koller.shared.R.drawable.close_thick)
+            chip.setOnCloseIconClickListener{
+                chip.closeIcon = AppCompatResources.getDrawable(requireContext(), com.norbert.koller.shared.R.drawable.arrow_drop)
+                chip.text = getString(com.norbert.koller.shared.R.string.date)
+                chip.tag = null
+                chip.isCheckable = true
+                chip.isChecked = false
+                chip.isCheckable = false
+                chip.setOnCloseIconClickListener(null)
+            }
+        }
+
+
+
+
+        drpd.show(parentFragmentManager,  "MATERIAL_DATE_RANGE_PICKER")
+
+        return drpd
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
 
         return inflater.inflate(R.layout.fragment_calendar_base_programs, container, false)
     }
@@ -49,6 +92,13 @@ class CalendarBaseProgramsFragment : Fragment() {
         val chipLength : Chip = view.findViewById(R.id.chip_length)
 
         val chipGroupSort : ChipGroup = view.findViewById(Rs.id.chip_group_sort)
+
+        val chipDate : Chip = view.findViewById(R.id.chip_date)
+
+        chipDate.setOnClickListener{
+            setupDrpd(chipDate)
+        }
+
 
         val baseProgramRecycleAdapter = BaseProgramRecyclerAdapter(chipGroupSort, listOf(chipLength))
 
