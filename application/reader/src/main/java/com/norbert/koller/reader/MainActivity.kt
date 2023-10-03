d447789f00e05f9ecb6ff8c39ad246f33cc8d7e9
Marci599 <katona.m.b@gmail.com>
2023-10-03 21:53:12 +0200

    NFC olvasó implementálásának megkezdése

--- application/reader/src/main/java/com/norbert/koller/reader/MainActivity.kt
+++ application/reader/src/main/java/com/norbert/koller/reader/MainActivity.kt
@@ -1,11 +1,74 @@
 package com.norbert.koller.reader
 
+import android.app.PendingIntent
+import android.content.Intent
+import android.nfc.NdefMessage
+import android.nfc.NfcAdapter
+import android.nfc.Tag
+import android.nfc.tech.NfcA
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private var nfcAdapter: NfcAdapter? = null
+    private lateinit var pendingIntent: PendingIntent
+    private lateinit var textKey : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textKey = findViewById(R.id.text_key)
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+
+        val intent = Intent(this, javaClass).apply {
+            addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        }
+        pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        nfcAdapter?.enableForegroundDispatch(this, pendingIntent, null, null)
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        nfcAdapter?.disableForegroundDispatch(this)
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        handleNFCIntent(intent)
+    }
+
+    private fun handleNFCIntent(intent: Intent) {
+        Log.d("ASD", intent.action.toString())
+        if (NfcAdapter.ACTION_TAG_DISCOVERED == intent.action) {
+            val tag = intent.getParcelableExtra<Tag>(NfcAdapter.EXTRA_TAG)
+            if (tag != null) {
+                val nfcA = NfcA.get(tag)
+                nfcA.connect()
+                val data = nfcA.transceive(byteArrayOf(/* parancsok */))
+                nfcA.close()
+
+                val hexData = bytesToHex(data)
+                textKey.text = tag.toString()
+            }
+        }
+    }
+
+    private fun bytesToHex(bytes: ByteArray): String {
+        val stringBuilder = StringBuilder()
+        for (byte in bytes) {
+            stringBuilder.append(String.format("%02X", byte))
+        }
+        return stringBuilder.toString()
     }
 }
---
+++ application/reader/src/main/java/com/norbert/koller/reader/MyApplication.kt
@@ -0,0 +1,9 @@
+package com.norbert.koller.reader
+
+import android.app.Application
+
+class MyApplication : Application() {
+
+
+
+}
