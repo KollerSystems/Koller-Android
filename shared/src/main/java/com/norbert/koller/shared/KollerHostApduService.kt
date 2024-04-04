c507edbe92f4253f331a6f15ca7c6106c54082c8
Aniko <katona.m.b@gmail.com>
2024-04-05 01:43:31 +0200

    NFC kommunikáció

--- shared/src/main/java/com/norbert/koller/shared/KollerHostApduService.kt
+++ shared/src/main/java/com/norbert/koller/shared/KollerHostApduService.kt
@@ -2,12 +2,20 @@ package com.norbert.koller.shared
 
 import android.nfc.cardemulation.HostApduService
 import android.os.Bundle
+import com.norbert.koller.shared.data.UserData
+import com.norbert.koller.shared.managers.toBytes
+import com.norbert.koller.shared.managers.toInt
 
 class KollerHostApduService : HostApduService() {
 
+    companion object{
+        var handleNFC: ((uid : Int) -> Unit)? = null
+    }
+
     override fun processCommandApdu(commandApdu: ByteArray, extras: Bundle?): ByteArray {
 
-        return processCommandApdu(byteArrayOf(0x2E, 0x38), null)
+        handleNFC?.invoke(commandApdu.toInt())
+        return UserData.instance.uid.toBytes()
     }
 
     override fun onDeactivated(reason: Int) {
