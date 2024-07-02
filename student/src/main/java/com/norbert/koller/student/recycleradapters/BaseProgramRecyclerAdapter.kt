b30644b5cd4e5a429893b91ef0528f2cb2c2385b
Katona Márton <katona.m.b@gmail.com>
2024-07-02 16:51:27 +0200

    app háttér fixek

--- student/src/main/java/com/norbert/koller/student/recycleradapters/BaseProgramRecyclerAdapter.kt
+++ student/src/main/java/com/norbert/koller/student/recycleradapters/BaseProgramRecyclerAdapter.kt
@@ -1,14 +1,16 @@
 package com.norbert.koller.student.recycleradapters
 
 import androidx.fragment.app.FragmentActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.norbert.koller.shared.activities.MainActivity
 import com.norbert.koller.shared.data.BaseProgramData
 import com.norbert.koller.student.fragments.bottomsheet.BaseProgramDetailsFragment
 import com.norbert.koller.shared.fragments.bottomsheet.RoomOrderBottomSheet
 import com.norbert.koller.shared.recycleradapters.BaseProgramRecyclerAdapter
 
 class BaseProgramRecyclerAdapter : BaseProgramRecyclerAdapter() {
-    override fun onItemPress(fragmentActivity: FragmentActivity, item : BaseProgramData) {
+    override fun onItemPress(holder: RecyclerView.ViewHolder, item : BaseProgramData) {
         val dialog = BaseProgramDetailsFragment(item)
-        dialog.show(fragmentActivity.supportFragmentManager, RoomOrderBottomSheet.TAG)
+        dialog.show((holder.itemView.context as MainActivity).supportFragmentManager, RoomOrderBottomSheet.TAG)
     }
 }
