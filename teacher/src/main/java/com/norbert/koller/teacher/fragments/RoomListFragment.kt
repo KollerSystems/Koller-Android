7953022d3e9c32a89dd97e699cff081fe714eb38
Marci599 <katona.m.b@gmail.com>
2024-12-14 18:31:58 +0100

    Szobák létrehozása variálás

--- teacher/src/main/java/com/norbert/koller/teacher/fragments/RoomListFragment.kt
+++ teacher/src/main/java/com/norbert/koller/teacher/fragments/RoomListFragment.kt
@@ -5,7 +5,8 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.content.res.AppCompatResources
-import com.norbert.koller.shared.R
+import com.norbert.koller.shared.R as Rs
+import com.norbert.koller.teacher.R
 import com.norbert.koller.shared.data.ListCardItem
 import com.norbert.koller.shared.fragments.bottomsheet.ListBsdfFragment
 import com.norbert.koller.shared.fragments.bottomsheet.ListCardStaticBsdfFragment
@@ -17,12 +18,12 @@ class RoomListFragment() : com.norbert.koller.shared.fragments.RoomListFragment(
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        addButton("Helyiségek létrehozása"){
+        addButton(getString(R.string.create_places)){
             val dialog = ListCardStaticBsdfFragment().setup((context as AppCompatActivity), arrayListOf(
                 ListCardItem(
-                    getString(R.string.rooms),
+                    getString(Rs.string.rooms),
                     null,
-                    AppCompatResources.getDrawable(requireContext(), R.drawable.room), {
+                    AppCompatResources.getDrawable(requireContext(), Rs.drawable.room), {
                         val intent = Intent(requireContext(), CreatePlacesActivity::class.java)
                         intent.putExtra("type", CreatePlacesActivity.ROOM)
                         requireContext().startActivity(intent)
